eeebcb3bcf3505a7e9a75e9adbf5ae11cf40b0c0
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-09-26 23:16:08 -0700

    initial commit

---
+++ Exercise2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        public static float Add(float[] numberArray)
+        {
+            var total = numberArray[0];
+            foreach (var number in numberArray.Skip(1))
+            {
+                total += number;
+            }
+
+            return total;
+        }
+        public static float Sub(float[] numberArray)
+        {
+            var total = numberArray[0];
+            foreach (var number in numberArray.Skip(1))
+            {
+                total -= number;
+            }
+
+            return total;
+        }
+        public static float Pro(float[] numberArray)
+        {
+            var total = numberArray[0];
+            foreach (var number in numberArray.Skip(1))
+            {
+                total *= number;
+            }
+
+            return total;
+        }
+        public static float Quo(float[] numberArray)
+        {
+            var total = numberArray[0];
+            foreach (var number in numberArray.Skip(1))
+            {
+                total /= number;
+            }
+
+            return total;
+        }
+
+        // Driver Code	
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a list of any length of numbers to add, subtract, multiply, and divide them all" +
+                " in sequential order." +
+            "\ni.e. 1, 2, 3, 4, 5");
+
+            var numbers = Console.ReadLine();
+
+            var numberArray = numbers
+                .Split(",")
+                .Select(x => float.Parse(x))
+                .ToArray();
+
+            var sum = Add(numberArray);
+            var difference = Sub(numberArray);
+            var product = Pro(numberArray);
+            var quotient = Quo(numberArray);
+
+            // Displaying result
+            Console.WriteLine($"Sum of given sequence:\n       {sum}");
+            Console.WriteLine($"Difference of given sequence:\n       {difference}");
+            Console.WriteLine($"Product of given sequence:\n       {product}");
+            Console.WriteLine($"Quotient of given sequence:\n       {quotient}");
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+            Console.WriteLine($"Once upon a time, {name} had a pet {favoriteAnimal} that was the color {favoriteColor}. " +
+                $"\n{name}'s amazing {favoriteAnimal} became the mascot of {favoriteBand}.\nThe end.");
         }
     }
 }
